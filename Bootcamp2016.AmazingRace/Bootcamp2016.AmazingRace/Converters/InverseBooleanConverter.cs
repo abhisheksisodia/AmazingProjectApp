77cb62b99a04be2cb48523858fe7ed9cf7a2d877
Abhishek Sisodia <asisodia@infusion.com>
2016-06-24 14:25:51 -0400

    Added join team implementation

---
+++ Bootcamp2016.AmazingRace/Bootcamp2016.AmazingRace/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bootcamp2016.AmazingRace.Converters
+{
+    public class InvertBooleanConverter : IValueConverter
+    {
+
+        #region IValueConverter implementation
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+            {
+
+                return !(bool)value;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
--- Bootcamp2016.AmazingRace/Bootcamp2016.AmazingRace/ViewModels/LoginViewModel.cs
+++ Bootcamp2016.AmazingRace/Bootcamp2016.AmazingRace/ViewModels/LoginViewModel.cs
@@ -3,6 +3,8 @@
 using Bootcamp2016.AmazingRace.Services;
 using Caliburn.Micro.Xamarin.Forms;
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -15,54 +17,62 @@ namespace Bootcamp2016.AmazingRace.ViewModels
     {
         private readonly INavigationService _navService;
         private readonly IAuthenticationService _authService;
+        private readonly IDataService _dataService;
+        private readonly ISettingsService _settingsService;
         private bool _isAuthenticated;
+        private string _teamName;
 
-        public LoginViewModel(IAuthenticationService authService, INavigationService navService)
+        public LoginViewModel(IAuthenticationService authService, INavigationService navService,
+                              IDataService dataService, ISettingsService settingsService)
         {
+            _settingsService = settingsService;
             _navService = navService;
             _authService = authService;
+            _dataService = dataService;
             LoginCommand = new Command(Login);
+            JoinCommand = new Command(Join);
         }
         
         public ICommand LoginCommand { get; set; }
-
+        public ICommand JoinCommand { get; set; }
+        
         public bool IsAuthenticated
         {
             get { return _isAuthenticated; }
             set { SetField(ref _isAuthenticated, value); }
         }
 
+        public string TeamName
+        {
+            get { return _teamName; }
+            set { SetField(ref _teamName, value); }
+        }
+
+        //called to authenticate user
         async void Login()
         {
-            await _authService.LoginAsync();
+            IsAuthenticated = await _authService.LoginAsync();
+        }
+
+        private async void Join()
+        {
+            if (string.IsNullOrEmpty(TeamName))
+                return;
+            await _dataService.JoinTeamAsync(TeamName);
+
+            var races = await _dataService.GetRacesAsync();
+            if (races.Any())
+                _settingsService.RaceId = races.First().Id;
+
             await _navService.NavigateToViewModelAsync<TabbedViewModel>();
         }
 
-        async void isLoggedIn()
-        {
-            IsAuthenticated = await _authService.IsLoggedInAsync();
-        }
 
-        protected override void OnInitialize()
+        protected async override void OnInitialize()
         {
             base.OnInitialize();
-            isLoggedIn();
-            if (!IsAuthenticated)
-            {
-                Login();
-            }
-            _navService.NavigateToViewModelAsync<TabbedViewModel>();
-        }
-
-        protected override void OnActivate()
-        {
-            base.OnActivate();
-        }
-
-        protected override void OnDeactivate(bool close)
-        {
-            base.OnDeactivate(close);
-        }
+            IsAuthenticated = await _authService.IsLoggedInAsync();
+        }       
 
     }
 }
